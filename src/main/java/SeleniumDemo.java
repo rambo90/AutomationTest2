238f8c498d3b6ac03e7cbca05f1de9fdacd9a25b
Elchupabre <rambo1946@gmail.com>
2017-04-15 19:01:02 +0300

    Add spaces

--- src/main/java/SeleniumDemo.java
+++ src/main/java/SeleniumDemo.java
@@ -2,7 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.util.List;
+
 import static java.lang.Thread.sleep;
 
 public class SeleniumDemo {
